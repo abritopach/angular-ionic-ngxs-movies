00283a141eff685d3ad4d3ddca08fe6915a5cb64
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2018-05-11 12:16:27 +0200

    Updated package dependencies

--- src/app/providers/movies-service.ts
+++ src/app/providers/movies-service.ts
@@ -10,7 +10,7 @@ import { Movie } from '../models/movie.model';
 export class MoviesService {
 
   // private readonly URL_BASE: string = 'http://localhost:3000/';
-  private readonly URL_BASE: string = 'https://8e7360dd.ngrok.io/';
+  private readonly URL_BASE: string = 'https://25133e91.ngrok.io/';
 
   constructor(private http: HttpClient) {
   }
