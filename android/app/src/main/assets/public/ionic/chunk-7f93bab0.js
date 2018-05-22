bee42bdb3e5075848e0a452f7e5ee99a9cc42a6b
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2018-05-22 12:56:00 +0200

    Updated to ionic/angular 4.0.0-alpha.7

--- android/app/src/main/assets/public/ionic/chunk-7f93bab0.js
+++
@@ -1,5 +0,0 @@
-/*!
- * (C) Ionic http://ionicframework.com - MIT License
- * Built with http://stenciljs.com
- */
-const{h:n}=window.Ionic;function t(n,t,e){return Math.max(n,Math.min(t,e))}function e(n,t){if(!n){const n="ASSERT: "+t;throw console.error(n),new Error(n)}}function r(n){return n.timeStamp||Date.now()}function o(n){if(n){const t=n.changedTouches;if(t&&t.length>0){const n=t[0];return{x:n.clientX,y:n.clientY}}if(void 0!==n.pageX)return{x:n.pageX,y:n.pageY}}return{x:0,y:0}}function i(n,t){const e="rtl"===n.document.dir;switch(t){case"start":return e;case"end":return!e;default:throw new Error(`"${t}" is not a valid value for [side]. Use "start" or "end" instead.`)}}function a(n){return c(n,0)}function c(n,t){const e=n._original||n;return{_original:n,emit:function(n,t=0){let e;return(...r)=>{clearTimeout(e),e=setTimeout(n,t,...r)}}(e.emit.bind(e),t)}}export{r as a,o as b,a as c,t as d,e,c as f,i as g};
