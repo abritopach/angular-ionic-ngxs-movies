da1fbe9d925203d6a79abe1561c7c7f834789656
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2018-06-19 11:15:06 +0200

    Updated

--- src/app/providers/movies-service.ts
+++ src/app/providers/movies-service.ts
@@ -10,7 +10,7 @@ import { Movie } from '../models/movie.model';
 export class MoviesService {
 
   // private readonly URL_BASE: string = 'http://localhost:3000/';
-  private readonly URL_BASE: string = 'https://6e4d4b0b.ngrok.io/';
+  private readonly URL_BASE: string = 'https://0145339f.ngrok.io/';
 
   constructor(private http: HttpClient) {
   }
