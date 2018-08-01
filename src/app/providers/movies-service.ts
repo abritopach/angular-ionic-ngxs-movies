ec9180bf315277277f097daa5725072680664af3
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2018-08-01 10:51:49 +0200

    Fix problem with ion-buttons in toolbar

--- src/app/providers/movies-service.ts
+++ src/app/providers/movies-service.ts
@@ -10,7 +10,7 @@ import { Movie } from '../models/movie.model';
 export class MoviesService {
 
   // private readonly URL_BASE: string = 'http://localhost:3000/';
-  private readonly URL_BASE: string = 'https://63a02c21.ngrok.io/';
+  private readonly URL_BASE: string = 'https://7beddacc.ngrok.io/';
 
   constructor(private http: HttpClient) {
   }
