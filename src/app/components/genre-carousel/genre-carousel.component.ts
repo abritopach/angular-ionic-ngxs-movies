a62c75d36f8b36ad10c95c220fb5e2f2a71de9c2
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2018-09-03 17:37:32 +0200

    Updated

--- src/app/components/genre-carousel/genre-carousel.component.ts
+++ src/app/components/genre-carousel/genre-carousel.component.ts
@@ -11,18 +11,18 @@ export class GenreCarouselComponent implements OnInit {
 
   constructor() {
     this.genres = [
-      {src: 'assets/movies-genres/image1.png'},
-      {src: 'assets/movies-genres/image2.png'},
-      {src: 'assets/movies-genres/image3.png'},
-      {src: 'assets/movies-genres/image4.png'},
-      {src: 'assets/movies-genres/image5.png'},
-      {src: 'assets/movies-genres/image6.png'},
-      {src: 'assets/movies-genres/image7.png'},
-      {src: 'assets/movies-genres/image8.png'},
-      {src: 'assets/movies-genres/image9.png'},
-      {src: 'assets/movies-genres/image10.png'},
-      {src: 'assets/movies-genres/image11.png'},
-      {src: 'assets/movies-genres/image12.png'},
+      {text: 'Science fiction', src: 'assets/movies-genres/image1.png'},
+      {text: 'Hola', src: 'assets/movies-genres/image2.png'},
+      {text: 'Crime', src: 'assets/movies-genres/image3.png'},
+      {text: 'Romance', src: 'assets/movies-genres/image4.png'},
+      {text: 'Hola', src: 'assets/movies-genres/image5.png'},
+      {text: 'Drama', src: 'assets/movies-genres/image6.png'},
+      {text: 'Animation', src: 'assets/movies-genres/image7.png'},
+      {text: 'Hola', src: 'assets/movies-genres/image8.png'},
+      {text: 'Westerns', src: 'assets/movies-genres/image9.png'},
+      {text: 'Horror', src: 'assets/movies-genres/image10.png'},
+      {text: 'Hola', src: 'assets/movies-genres/image11.png'},
+      {text: 'Hola', src: 'assets/movies-genres/image12.png'},
     ];
   }
 
