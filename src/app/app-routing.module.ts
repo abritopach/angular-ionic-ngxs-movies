e60027069b79856f2dfe9506cfa6fe4b7e9ad161
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2018-09-04 09:58:33 +0200

    Updated genre carousel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [/*RouterModule.forRoot(routes)*/RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
--- src/app/components/genre-carousel/genre-carousel.component.ts
+++ src/app/components/genre-carousel/genre-carousel.component.ts
@@ -12,17 +12,17 @@ export class GenreCarouselComponent implements OnInit {
   constructor() {
     this.genres = [
       {text: 'Science fiction', src: 'assets/movies-genres/image1.png'},
-      {text: 'Hola', src: 'assets/movies-genres/image2.png'},
+      {text: 'Westerns', src: 'assets/movies-genres/image2.png'},
       {text: 'Crime', src: 'assets/movies-genres/image3.png'},
       {text: 'Romance', src: 'assets/movies-genres/image4.png'},
-      {text: 'Hola', src: 'assets/movies-genres/image5.png'},
+      {text: 'Comedy', src: 'assets/movies-genres/image5.png'},
       {text: 'Drama', src: 'assets/movies-genres/image6.png'},
       {text: 'Animation', src: 'assets/movies-genres/image7.png'},
-      {text: 'Hola', src: 'assets/movies-genres/image8.png'},
-      {text: 'Westerns', src: 'assets/movies-genres/image9.png'},
-      {text: 'Horror', src: 'assets/movies-genres/image10.png'},
-      {text: 'Hola', src: 'assets/movies-genres/image11.png'},
-      {text: 'Hola', src: 'assets/movies-genres/image12.png'},
+      {text: 'Action', src: 'assets/movies-genres/image8.png'},
+      {text: 'Adventure', src: 'assets/movies-genres/image9.png'},
+      {text: 'Thriller', src: 'assets/movies-genres/image10.png'},
+      {text: 'Magic/Fantasy', src: 'assets/movies-genres/image11.png'},
+      {text: 'Horror', src: 'assets/movies-genres/image12.png'},
     ];
   }
 
