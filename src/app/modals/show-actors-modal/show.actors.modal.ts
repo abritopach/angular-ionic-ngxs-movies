0fa3544cfe01d98af4537ad172c6837a2285194c
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2018-11-16 12:39:10 +0100

    Refactor code

--- src/app/modals/show-actors-modal/show.actors.modal.ts
+++ src/app/modals/show-actors-modal/show.actors.modal.ts
@@ -1,5 +1,6 @@
+import { forkJoin } from 'rxjs';
 import { Component, ViewEncapsulation, OnInit, AfterViewInit } from '@angular/core';
-import { ModalController, NavParams} from '@ionic/angular';
+import { ModalController, NavParams, LoadingController} from '@ionic/angular';
 
 import { SearchImageService } from './../../providers/search-image-service';
 
@@ -12,17 +13,22 @@ import { SearchImageService } from './../../providers/search-image-service';
 export class ShowActorsModalComponent implements OnInit {
 
   actors: any = [];
+  loading: any;
 
-  constructor(private modalCtrl: ModalController, private navParams: NavParams, private searchImageService: SearchImageService) {
+  constructor(private modalCtrl: ModalController, private navParams: NavParams, private searchImageService: SearchImageService,
+              private loadingCtrl: LoadingController) {
     console.log('ShowActorsModalComponent::constructor | method called');
   }
 
   ngOnInit() {
     console.log('ShowActorsModalComponent::ngOnInit | method called');
+
+    this.presentLoading();
     let actors = this.navParams.data.modalProps.actors;
     actors = actors.split(',');
     // console.log('actors', actors);
 
+    /*
     actors.map(actor => {
       // Code to use Google Custom Search Api.
       this.searchImageService.searchImage(actor)
@@ -30,8 +36,26 @@ export class ShowActorsModalComponent implements OnInit {
         if (result.items.length > 0) {
           // console.log(result);
           this.actors.push({name: actor, image: result.items[0].image.thumbnailLink});
+          this.dismissLoading();
         }
       });
+    });
+    */
+
+    forkJoin(
+      actors.map(actor => {
+        // Code to use Google Custom Search Api.
+        return this.searchImageService.searchImage(actor);
+      })
+    ).subscribe(results => {
+      // console.log(results);
+      if (results.length > 0) {
+        results.map((result, index) => {
+          // console.log(result);
+          this.actors.push({name: actors[index], image: result['items'][0].image.thumbnailLink});
+        });
+        this.dismissLoading();
+      }
     });
   }
 
@@ -40,6 +64,18 @@ export class ShowActorsModalComponent implements OnInit {
     // can "dismiss" itself and pass back data.
     // console.log('dismiss', data);
     this.modalCtrl.dismiss();
+  }
+
+  async presentLoading() {
+    this.loading = await this.loadingCtrl.create({
+      message: 'Please wait, loading actors...',
+    });
+
+    return await this.loading.present();
+  }
+
+  async dismissLoading() {
+    this.loading.dismiss();
   }
 
 }
