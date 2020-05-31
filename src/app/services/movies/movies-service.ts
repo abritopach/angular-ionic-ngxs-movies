5e6755faa52a4dca03e888192567a48b222b247b
abritopach <adrbrpa1988@gmail.com>
2020-05-31 19:19:03 +0200

    Updated package dependencies

--- src/app/services/movies/movies-service.ts
+++ src/app/services/movies/movies-service.ts
@@ -10,7 +10,7 @@ import { Movie } from '../../models/movie.model';
 export class MoviesService {
 
   // private readonly URL_BASE: string = 'http://localhost:3000/';
-  private readonly URL_BASE: string = 'https://6e40564c.ngrok.io/';
+  private readonly URL_BASE: string = 'https://acf2972ccfcf.ngrok.io/';
 
   constructor(private http: HttpClient) {
   }
