d0c734f9546f0177852cb7dd5d001c40c15fc74e
abritopach <adrbrpa1988@gmail.com>
2022-03-07 22:55:56 +0000

    ✨ Added lintstagedrc.js file

---
+++ .lintstagedrc.js
@@ -0,0 +1,19 @@
+const { ESLint } = require('eslint')
+​
+const removeIgnoredFiles = async (files) => {
+    const eslint = new ESLint()
+    const isIgnored = await Promise.all(
+        files.map((file) => {
+        return eslint.isPathIgnored(file)
+        })
+    )
+        const filteredFiles = files.filter((_, i) => !isIgnored[i])
+        return filteredFiles.join(' ')
+    }
+​
+module.exports = {
+    'src/**/*.{js,jsx,ts,tsx,html,css,scss}': async (files) => {
+        const filesToLint = await removeIgnoredFiles(files)
+        return [`npx prettier --write ${filesToLint}`, `npx eslint ${filesToLint}`]
+    },
+}
